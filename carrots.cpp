d8ce42e5b9df8c40d25ba4ec8969e3b9672ff064
Clíodhna Harrison <cliodhnaharrison@gmail.com>
2017-09-14 16:17:15 +0100

    Add solving for carrots in C++

---
+++ carrots.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main(void)
+{
+  int x, y;
+  std::cin >> x >> y;
+  for (int i = 0; i < x; i ++){
+    std::string name;
+    std::cin >> name;
+  }
+  std::cout << y;
+  return 0;
+}
